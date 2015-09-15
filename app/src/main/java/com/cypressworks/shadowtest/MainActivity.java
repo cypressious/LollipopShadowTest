94d5525573ea5d193b3514732b7fe258991652ef
Kirill Rakhman <kirill.rakhman@gmail.com>
2015-09-15 21:14:03 +0200

    stuff

--- app/src/main/java/com/cypressworks/shadowtest/MainActivity.java
+++ app/src/main/java/com/cypressworks/shadowtest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cypressworks.shadowtest;
 
+import android.graphics.Outline;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        getSupportFragmentManager().beginTransaction().replace(R.id.content1,
-//                                                               new Fragment1()).commit();
+        ViewGroup layout = (ViewGroup) findViewById(R.id.layoutContent1);
+
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View v = layout.getChildAt(i);
+            v.setOutlineProvider(new ViewOutlineProvider() {
+                @Override
+                public void getOutline(final View view, final Outline outline) {
+                    outline.setRect(0, -128, view.getWidth(), view.getHeight());
+                }
+            });
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
